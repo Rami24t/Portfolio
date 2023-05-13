5bc53b3c6518da273c36dfecdbe00219f86f507c
RamS24 <AlSaadi.Rami24@gmail.com>
2023-05-13 14:53:22 +0200

    add explanations

---
+++ explanations.js
@@ -0,0 +1,9 @@
+// Description of some of the packages used in this project:
+// 1. --legacy-peer-deps: This is a temporary fix for the peer dependencies issue with npm 7. It will be removed when the issue is fixed.
+// 2. @react-three/fiber: React-three-fiber is a React renderer for threejs on the web and react-native.
+// 3. @react-three/drei: Drei is a growing collection of useful helpers and abstractions for react-three-fiber.
+// 4. maath: A simple, lightweight, and fully customizable math library for JavaScript.
+// 5. react-tilt: A tiny 60+fps parallax tilt hover effect for React.
+// 6. react-vertical-timeline-component: A vertical timeline for React.js.
+// 7. framer-motion: An open source React library to power production-ready animations.
+// 8. react-router-dom: DOM bindings for React Router.
