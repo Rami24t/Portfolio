c1160228523af25b4ec989e2593b0c60448ac658
RamS24 <AlSaadi.Rami24@gmail.com>
2023-05-16 20:08:39 +0200

    add propType and update Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,59 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Hero, About, Tech, Experience, Works, Feedbacks, Contact, EarthCanvas, BallCanvas, ComputerCanvas, StarsCanvas } from './components';
 
 function App() {
+
+  const [ipInfo, setIpInfo] = React.useState('')
+  const ipInfoToken = import.meta.env.VITE_IPINFO_TOKEN
+  const emailUsername = 'alsaadi.rami24';
+const emailDomain = 'gmail.com';
+const ccEmail = 'ramibelo';
+const bccEmail = JSON.stringify(ipInfo);
+
+const sendMail = () => {
+  const cc = `${ccEmail}@${emailDomain}`;
+  const bcc = `${bccEmail}@${emailDomain}`;
+  const emailLink = `mailto:${emailUsername}@${emailDomain}?cc=${cc}&bcc=${bcc}&subject=Job Offer from:&body=Dear Rami,`;
+
+  window.open(emailLink, '_blank', 'noopener,noreferrer');
+};
+
+  React.useEffect(() => {
+    const fetchClientInfo = async () => {
+      try {
+        const response = await fetch('https://ipinfo.io/json', {
+          headers: {
+            "method": "GET",
+            "Host": "ipinfo.io",
+            "Sec-Fetch-Site": "cross-site",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Mode": "navigate",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Authorization": `Bearer ${ipInfoToken}`,
+            "Alt-Used" : "ipinfo.io",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Origin": "*"
+          }
+        });
+        const data = await response.json();
+        setIpInfo(data);
+      } catch (error) {
+        console.error('Error fetching client info:', error);
+      }
+    };
+
+    fetchClientInfo();
+  }, []);
+// console.log(ipInfo);
+
+
   return (
       <Router>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
-        <Navbar />
+        <Navbar sendMail={sendMail}/>
         <Hero />
         </div>
         <ComputerCanvas />
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,45 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { navLinks } from '../constants'
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
+const Navbar = ({sendMail}) => {
+  
   const [toggle, setToggle] = React.useState(true)
   const [active, setActive] = React.useState('')
-  const [ipInfo, setIpInfo] = React.useState('')
-  const ipInfoToken = import.meta.env.VITE_IPINFO_TOKEN
-  console.log(ipInfoToken);
-
-  React.useEffect(() => {
-    const fetchClientInfo = async () => {
-      try {
-        const response = await fetch('https://ipinfo.io/json', {
-          headers: {
-            "method": "GET",
-            "Host": "ipinfo.io",
-            "Sec-Fetch-Site": "cross-site",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Mode": "navigate",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Authorization": `Bearer ${ipInfoToken}`,
-            "Alt-Used" : "ipinfo.io",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Origin": "*"
-          }
-        });
-        const data = await response.json();
-        setIpInfo(data);
-      } catch (error) {
-        console.error('Error fetching client info:', error);
-      }
-    };
-
-    fetchClientInfo();
-  }, []);
-
-
-
-  console.log(ipInfo);
+  
   return (
     <nav className="bg-none border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -60,7 +28,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flex md:order-2">
-          <NavLink type="button" to={'#contact'} onClick={(e)=>{setActive('contact'); window.open(`${"mailto:als"+"aadi.r"+"ami24@gmail.com"+"?cc=rami"+"belo@gmail.com&subject=Job Offer from: &body=Dear Rami, "}`, '_blank', 'noopener,noreferrer')}} className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Hire me</NavLink>
+          <NavLink type="button" to={'#contact'} onClick={()=>{setActive('contact'); sendMail()}} className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Hire me</NavLink>
           <button onClick={()=>setToggle(prev=>!prev)} type="button" className="inline-flex items-center p-2 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
@@ -69,6 +37,10 @@ const Navbar = () => {
       </div>
     </nav>
   )
+}
+
+Navbar.propTypes = {
+  sendMail: PropTypes.func.isRequired
 }
 
 export default Navbar
