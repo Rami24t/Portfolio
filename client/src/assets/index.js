fda0d24eab48d2d8813455a9d0a58bfae2975698
Rami <alsaadi.rami24@gmail.com>
2023-11-22 13:28:15 +0100

    edit logo and add 8 more project images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,10 +3,18 @@ import alfatraining from "./institute/alfatraining.png";
 import dci from "./institute/dci.svg";
 import alkis from "./other/alkis.jpg";
 import inosoft from "./institute/inosoft.png";
+import lettersOnFire from "./projects/LettersOnFire_CL-guessing-game/lof2.png";
+import lettersOnFire2 from "./projects/LettersOnFire_CL-guessing-game/lof3.png";
+import sM1 from "./projects/SM/SM2.png";
+import sM2 from "./projects/SM/SM1.jpg";
+import gsl from "./projects/gsl/gsl.jpg";
+import gsl2 from "./projects/gsl/gsl2.jpg";
 import socialapp1 from "./projects/SocialWallPostsApp/Social-Wall_Profile.png";
 import cssillustrator1 from "./projects/CSS-Illustrator/CSS-Illustrator1.png";
+import bubbles from "./projects/Bubbles/bubbles.png";
 import restaurant1 from "./projects/IRT_E-Restaurant/add.png";
-import logo from "./rami-logo/svg/logo-color.svg"
+import cat from "./projects/IRT_E-Restaurant/cat.png";
+import logo from "/logo2-no-background.svg";
 import backend from "./other/backend.png";
 import creator from "./other/creator.png";
 import mobile from "./other/mobile.png";
@@ -27,9 +35,17 @@ import threejs from "./tech/threejs.svg";
 
 export {
   logo,
+  lettersOnFire,
+  lettersOnFire2,
+  gsl,
+  gsl2,
   socialapp1,
   cssillustrator1,
+  bubbles,
   restaurant1,
+  cat,
+  sM1,
+  sM2,
   backend,
   creator,
   mobile,
