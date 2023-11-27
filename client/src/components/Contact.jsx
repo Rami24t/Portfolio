f7b21a0f523e45d473e4ef82e7bdcbab9d06d398
Rami <alsaadi.rami24@gmail.com>
2023-11-28 00:04:59 +0100

    remove dev alert

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -29,10 +29,6 @@ export const Contact = SectionWrapper(() => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    alert(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID +
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID
-    );
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
