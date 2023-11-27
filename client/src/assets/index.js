5fb6449cfdcbf126a400793c5f5878f1b28f7f6a
Rami <alsaadi.rami24@gmail.com>
2023-11-27 23:20:17 +0100

    index new assets

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,6 +1,7 @@
 import aou from "./institute/aou.png";
 import alfatraining from "./institute/alfatraining.png";
 import dci from "./institute/dci.svg";
+import fcc from "./institute/fcc.jpg";
 import alkis from "./other/alkis.jpg";
 import inosoft from "./institute/inosoft.png";
 import lettersOnFire from "./projects/LettersOnFire_CL-guessing-game/lof2.png";
@@ -10,17 +11,30 @@ import sM2 from "./projects/SM/SM1.jpg";
 import gsl from "./projects/gsl/gsl.jpg";
 import gsl2 from "./projects/gsl/gsl2.jpg";
 import socialapp1 from "./projects/SocialWallPostsApp/Social-Wall_Profile.png";
+import socialapp2 from "./projects/SocialWallPostsApp/Social-Wall_Posts.jpg";
 import cssillustrator1 from "./projects/CSS-Illustrator/CSS-Illustrator1.png";
+import cssillustrator2 from "./projects/CSS-Illustrator/CSS-Illustrator2.png";
+import socialUI1 from "./projects/SocialUI/SocialUI1.png";
+import socialUI2 from "./projects/SocialUI/SocialUI2.png";
 import bubbles from "./projects/Bubbles/bubbles.png";
+import bubbles2 from "./projects/Bubbles/bubbles2.jpg";
+import gnr1 from "./projects/GitNewRemoteHelper/gnr1.png";
+import gnr2 from "./projects/GitNewRemoteHelper/gnr2.png";
+import ttt1 from "./projects/ttt/ttt1.png";
+import ttt2 from "./projects/ttt/ttt2.jpg";
+import aoa2 from "./projects/AzureOpenAI/aoa1.jpg";
+import aoa1 from "./projects/AzureOpenAI/aoa2.jpg";
+import st1 from "./projects/Social-Ticketing/st6.jpg";
+import st2 from "./projects/Social-Ticketing/st5.jpg";
 import restaurant1 from "./projects/IRT_E-Restaurant/add.png";
 import cat from "./projects/IRT_E-Restaurant/cat.png";
+import edmundo1 from "./projects/Edmundo/edmundo3.jpg";
+import edmundo2 from "./projects/Edmundo/edmundo1.png";
 import logo from "/logo2-no-background.svg";
 import backend from "./other/backend.png";
 import creator from "./other/creator.png";
 import mobile from "./other/mobile.png";
 import web from "./other/web.png";
-import menu from "./other/menu.svg";
-import close from "./other/close.svg";
 import github from "./other/github.png";
 import css from "./tech/css.png";
 import html from "./tech/html.png";
@@ -40,10 +54,25 @@ export {
   gsl,
   gsl2,
   socialapp1,
+  socialapp2,
+  socialUI1,
+  socialUI2,
   cssillustrator1,
+  cssillustrator2,
   bubbles,
+  bubbles2,
+  gnr1,
+  gnr2,
   restaurant1,
   cat,
+  ttt1,
+  ttt2,
+  st1,
+  st2,
+  edmundo1,
+  edmundo2,
+  aoa1,
+  aoa2,
   sM1,
   sM2,
   backend,
@@ -51,8 +80,6 @@ export {
   mobile,
   web,
   github,
-  menu,
-  close,
   css,
   git,
   html,
@@ -64,6 +91,7 @@ export {
   typescript,
   threejs,
   dci,
+  fcc,
   alkis,
   inosoft,
   alfatraining,
