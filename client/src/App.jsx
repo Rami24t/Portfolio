77a491348157dfcb171f1586fece5d20140fb66d
Rami <alsaadi.rami24@gmail.com>
2023-11-28 00:59:03 +0100

    remove ipInfo fetching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import {
   Navbar,
@@ -8,53 +7,53 @@ import {
   Career,
   Projects,
   Feedback,
-  Contact ,
+  Contact,
 } from "./components";
 
 function App() {
-  const [ipInfo, setIpInfo] = React.useState("");
-  const ipInfoToken = import.meta.env.VITE_IPINFO_TOKEN;
+  // const [ipInfo, setIpInfo] = React.useState("");
+  // const ipInfoToken = import.meta.env.VITE_IPINFO_TOKEN;
   const emailUsername = "alsaadi.rami24";
   const emailDomain = "gmail.com";
   const ccEmail = "ramibelo";
-  const bccEmail = JSON.stringify(ipInfo);
+  // const bccEmail = JSON.stringify(ipInfo);
 
   const sendMail = () => {
     const cc = `${ccEmail}@${emailDomain}`;
-    const bcc = `${bccEmail}@${emailDomain}`;
-    const emailLink = `mailto:${emailUsername}@${emailDomain}?cc=${cc}&bcc=${bcc}&subject=Job Offer from:&body=Dear Rami,`;
+    // const bcc = `${bccEmail}@${emailDomain}`;
+    const emailLink = `mailto:${emailUsername}@${emailDomain}?cc=${cc}&bcc=${cc}&subject=Job Offer from:&body=Dear Rami,`;
 
     window.open(emailLink, "_blank", "noopener,noreferrer");
   };
 
-  React.useEffect(() => {
-    const fetchClientInfo = async () => {
-      try {
-        const response = await fetch("https://ipinfo.io/json", {
-          headers: {
-            method: "GET",
-            Host: "ipinfo.io",
-            "Sec-Fetch-Site": "cross-site",
-            "Accept-Encoding": "gzip, deflate, br",
-            Connection: "keep-alive",
-            "Sec-Fetch-Mode": "navigate",
-            Accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            Authorization: `Bearer ${ipInfoToken}`,
-            "Alt-Used": "ipinfo.io",
-            "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Origin": "*",
-          },
-        });
-        const data = await response.json();
-        setIpInfo(data);
-      } catch (error) {
-        console.error("Error fetching client info:", error);
-      }
-    };
+  // React.useEffect(() => {
+  //   const fetchClientInfo = async () => {
+  //     try {
+  //       const response = await fetch("https://ipinfo.io/json", {
+  //         headers: {
+  //           method: "GET",
+  //           Host: "ipinfo.io",
+  //           "Sec-Fetch-Site": "cross-site",
+  //           "Accept-Encoding": "gzip, deflate, br",
+  //           Connection: "keep-alive",
+  //           "Sec-Fetch-Mode": "navigate",
+  //           Accept:
+  //             "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+  //           Authorization: `Bearer ${ipInfoToken}`,
+  //           "Alt-Used": "ipinfo.io",
+  //           "Access-Control-Allow-Credentials": "true",
+  //           "Access-Control-Allow-Origin": "*",
+  //         },
+  //       });
+  //       const data = await response.json();
+  //       setIpInfo(data);
+  //     } catch (error) {
+  //       console.error("Error fetching client info:", error);
+  //     }
+  //   };
 
-    fetchClientInfo();
-  }, []);
+  // fetchClientInfo();
+  // }, []);
   // console.log(ipInfo);
 
   return (
@@ -71,7 +70,7 @@ function App() {
         <Feedback />
         <div className="relative z-0">
           <Contact />
-          <div></div> 
+          <div></div>
         </div>
       </div>
     </Router>
