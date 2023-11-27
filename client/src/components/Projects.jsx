c840339bf48bbddf3408831fda12ab8e7fa95ee3
Rami <alsaadi.rami24@gmail.com>
2023-11-28 00:43:11 +0100

    edit projects section

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,29 +1,33 @@
-import { motion } from "framer-motion";
-import { fadeIn, textVariant } from "../utils/motion";
-import { projects } from "../constants";
 import { SectionWrapper } from "../hoc";
-import tw from "../tw-styles";
+// import { motion } from "framer-motion";
+// import { fadeIn, textVariant } from "../utils/motion";
 import { ProjectCard } from "./ProjectCard";
+import { projects } from "../constants";
+import tw from "../tw-styles";
 
 export const Projects = SectionWrapper(() => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${tw.sectionSubText} `}>My work</p>
-        <h2 className={`${tw.sectionHeadText}`}>Projects</h2>
-      </motion.div>
+      <div>
+        {/* <motion.div variants={textVariant()}> */}
+        <p className={tw.sectionSubText}>My work</p>
+        <h2 className={tw.sectionHeadText}>Projects</h2>
+        {/* </motion.div> */}
+      </div>
 
       <div className="w-full flex">
-        <motion.p
+        {/* <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          Following projects showcases my skills and career through
+        > */}
+        <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          The following projects showcases my skills and career through
           practical examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </motion.p>
+        </p>
+        {/* </motion.p> */}
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
