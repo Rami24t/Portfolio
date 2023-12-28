0c6a82e8edc0e9e245c8b02ae0ca6228cd6c6f33
Rami <alsaadi.rami24@gmail.com>
2023-12-28 12:18:47 +0100

    edit vercel.json - api

---
+++ client/api/sitemap-redirect.js
@@ -0,0 +1,21 @@
+// api/sitemap-redirect.js
+
+module.exports = (req, res) => {
+  const host = req.headers.host || "";
+  const redirectMap = {
+    "ramidev.tech": "/sitemap-tech.xml",
+    "vercel.app": "/sitemap-app.xml",
+    // Add more host mappings as needed
+  };
+
+  const targetPath = redirectMap[host];
+
+  if (targetPath) {
+    res.writeHead(302, {
+      Location: targetPath,
+    });
+    res.end();
+  } else {
+    res.status(404).end();
+  }
+};
