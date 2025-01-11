d69d002aa7dc095c4392b68c4aaaa3fa9fffbac9
Rami <alsaadi.rami24@gmail.com>
2025-01-11 01:26:26 +0100

    fix: Career.jsx : ui: invisible title & code: unnamed export

--- client/src/components/Career.jsx
+++ client/src/components/Career.jsx
@@ -1,35 +1,27 @@
-import {
-  VerticalTimeline,
-} from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
-
-import "react-vertical-timeline-component/style.min.css";
-
 import tw from "../tw-styles";
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import { careers } from "../constants";
-import { SectionWrapper } from "../hoc";
-import { textVariant } from "../utils/motion";
 import { CareerCard } from "./CareerCard";
+import { SectionWrapper } from "../hoc";
 
-const Career = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={`${tw.sectionSubText} text-center`}>
-          What I have done so far
-        </p>
-        <h2 className={`${tw.sectionHeadText} text-center`}>Career Path</h2>
-      </motion.div>
+const Career = () => (
+  <>
+    <div>
+      <p className={`${tw.sectionSubText} text-center`}>
+        What I have done so far
+      </p>
+      <h2 className={`${tw.sectionHeadText} text-center`}>Career Path</h2>
+    </div>
+    <div className="mt-20 flex flex-col">
+      <VerticalTimeline>
+        {careers.map((career, index) => (
+          <CareerCard key={`career-${index}`} career={career} />
+        ))}
+      </VerticalTimeline>
+    </div>
+  </>
+);
 
-      <div className="mt-20 flex flex-col">
-        <VerticalTimeline>
-          {careers.map((career, index) => (
-            <CareerCard key={`career-${index}`} career={career} />
-          ))}
-        </VerticalTimeline>
-      </div>
-    </>
-  );
-};
-
-export default SectionWrapper(Career, "work");
+const wrappedCareerWorkSection = SectionWrapper(Career, "work");
+export default wrappedCareerWorkSection;
